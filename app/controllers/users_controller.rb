27208d1a408c3204f5af7ec028b0000b73f882d1
Yuuki Kawasaki <yuku08@gmail.com>
2016-08-24 13:11:01 +0000

    add edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
   
   def show
    @user = User.find(params[:id])
   end
   
   def new
-    @user = User.new
+   @user = User.new
   end
   
   def create
@@ -18,11 +19,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_url unless @user == current_user
   end
   
 end
